d149f43570b3c0a4a2b56787c3174b672b1decf6
noxoomo <noxoomo@yandex-team.ru>
2017-02-24 17:27:32 +0300

    python3 support; decompose simulator and CLI; fix strange behaviour in pure-trend simulations;

---
+++ simulation/run_simulator.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+import os.path
+import os
+
+import errno
+import numpy as np
+from simulator import *
+
+
+def smart_makedirs(dirname):
+    try:
+        os.makedirs(dirname)
+    except OSError as exc:
+        if exc.errno != errno.EEXIST:
+            raise exc
+
+
+def parse_cmd(*args, **kwargs):
+    import argparse
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-o", "--outdir",
+                        type=str,
+                        default=current_dir,
+                        help="Output directory")
+    parser.add_argument("-p", "--plot",
+                        action='store_const',
+                        const=True,
+                        help="Plot series")
+    parser.add_argument("-N", "--number_series",
+                        type=int,
+                        default=1,
+                        help="Total number of series to simulate")
+    parser.add_argument("-l", "--series_len",
+                        type=int,
+                        default=1000)
+    parser.add_argument("-t", "--trend_start",
+                        type=float,
+                        default=100,
+                        help="Starting point for trend")
+    parser.add_argument("-m", "--mean_tang",
+                        type=float,
+                        default=0,
+                        help="Mean of tangence for trend")
+    parser.add_argument("-v", "--var_tang",
+                        type=float,
+                        default=0.5,
+                        help="Varience of tangence for trend")
+    parser.add_argument("-s", "--var_noise",
+                        type=float,
+                        default=0.1,
+                        help="Varience of noise")
+    parser.add_argument("-e", "--exp_lamb",
+                        type=float,
+                        default=14,
+                        help="Intensity of moments of trend change")
+    parser.add_argument("--seed",
+                        type=int,
+                        default=int(np.random.randint(low=0,
+                                                      high=100000,
+                                                      size=1)[0]),
+                        help="Random seed")
+
+    return parser.parse_args()
+
+
+def dump_params(params):
+    import json
+    with open(os.path.join(params.outdir, "params.txt"), 'w') as f:
+        json.dump(vars(params), f, sort_keys=True, indent=4)
+
+
+def get_seed(params):
+    if params.seed is not None:
+        np.random.seed(params.seed)
+    params.seed = np.random.randint(low=0,
+                                    high=100000,
+                                    size=params.number_series)
+    # np.int is not json-serializable in python3
+    params.seed = [int(x) for x in params.seed]
+
+
+def prepare_output_dir(params):
+    smart_makedirs(params.outdir)
+
+
+def plot_series(series):
+    import plotly.graph_objs as go
+    from plotly.offline import plot
+
+    traces = []
+    for sample in series:
+        trace = go.Scatter(x=[x for x in range(1, len(sample) + 1)],
+                           y=sample,
+                           mode='lines')
+        traces.append(trace)
+    layout = {"title": "Series"}
+    fig = {"data": traces, "layout": layout}
+    plot(fig)
+
+
+def main(*args, **kwargs):
+    params = parse_cmd(*args, **kwargs)
+    prepare_output_dir(params)
+    get_seed(params)
+    dump_params(params)
+
+    sampler = SeriesSampler(tan_mean=params.mean_tang,
+                            tan_var=params.var_tang,
+                            intensity=params.exp_lamb,
+                            wiener_var=params.var_noise)
+
+    all_series = []
+    for i in range(params.number_series):
+        series = sampler.simulate(params.series_len, params.trend_start, seed=params.seed[i])
+        all_series.append(series)
+        np.savetxt(os.path.join(params.outdir, str(i) + ".txt"), series)
+
+    if params.plot:
+        plot_series(all_series)
+
+
+if __name__ == "__main__":
+    main()
--- simulation/simulator.py
+++ simulation/simulator.py
@@ -1,161 +1,51 @@
-import argparse
-import os.path
-import errno
-
 import numpy as np
 from scipy.stats import norm, expon
 
-current_dir = os.path.dirname(os.path.realpath(__file__))
+
+class SeriesSampler(object):
+    def __init__(self, tan_mean, tan_var, intensity, wiener_var):
+        self.__tan_mean = tan_mean
+        self.__tan_var = tan_var
+        self.__wiener_var = wiener_var
+        self.__lambda = intensity
+
+    def __sample_wiener(self, size):
+        sample = norm.rvs(scale=self.__wiener_var, size=size)
+        return np.cumsum(sample)
+
+    def __sample_trend(self, size, bias=0):
+
+        cursor = 0.0
+        cum_sum = bias
+        trend = []
+
+        while cursor < size:
+            previous = cursor
+            cursor += expon.rvs(scale=self.__lambda)
+            cursor = min(cursor, size)
+
+            tan = norm.rvs(loc=self.__tan_mean,
+                           scale=self.__tan_var)
+
+            left_idx = np.int(np.floor(previous))
+            right_idx = np.int(np.floor(cursor))
+
+            supp = np.arange(left_idx, right_idx) - previous
+
+            if len(supp) > 0:
+                trend.append(tan * supp + cum_sum)
+
+            cum_sum += tan * (cursor - previous)
+
+        trend = np.hstack(trend)
+        return trend
+
+    def simulate(self, series_size, trend_bias=0, seed=None):
+        if seed is not None:
+            np.random.seed(seed)
+        noise = self.__sample_wiener(series_size)
+        trend = self.__sample_trend(series_size, trend_bias)
+        return np.maximum(trend + noise, 0)
 
 
-def smart_makedirs(dirname):
-    try:
-        os.makedirs(dirname)
-    except OSError as exc:
-        if exc.errno != errno.EEXIST:
-            raise exc
 
-
-def get_wiener(N, sigma):
-    n = norm.rvs(scale=sigma, size=N)
-    return np.cumsum(n)
-
-
-def get_trend(N, lamb, m_tan, v_tan, sigma, tr_start):
-    left, right, cs = 0, 0, tr_start
-    trend = []
-
-    while right < N:
-        left = right
-        right += expon.rvs(scale=lamb)
-        right = min(right, N)
-        tan = norm.rvs(loc=m_tan, scale=v_tan)
-        fleft, fright = np.floor(left), np.floor(right)
-        supp = np.arange(fleft, fright) - fleft
-
-        cs += tan * (right - left)
-        if not len(supp):
-            continue
-
-        intercept = cs + tan * (left - fleft)
-        trend.append(tan * supp + intercept)
-
-    trend = np.hstack(trend)
-    return trend
-
-
-def get_series(trend, wiener):
-    series = trend + wiener
-    neg_ind = np.where(series < 0)[0]
-    if len(neg_ind):
-        series[neg_ind[0]:] = 0
-    return series
-
-
-def parse_cmd(*args, **kwargs):
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-o", "--outdir",
-                        type=str,
-                        default=current_dir,
-                        help="Output directory")
-    parser.add_argument("-p", "--plot",
-                        action='store_const',
-                        const=True,
-                        help="Plot series")
-    parser.add_argument("-N", "--number_series",
-                        type=int,
-                        default=1,
-                        help="Total number of series to simulate")
-    parser.add_argument("-l", "--series_len",
-                        type=int,
-                        default=1000)
-    parser.add_argument("-t", "--trend_start",
-                        type=float,
-                        default=100,
-                        help="Starting point for trend")
-    parser.add_argument("-m", "--mean_tang",
-                        type=float,
-                        default=0,
-                        help="Mean of tangence for trend")
-    parser.add_argument("-v", "--var_tang",
-                        type=float,
-                        default=5,
-                        help="Varience of tangence for trend")
-    parser.add_argument("-s", "--var_noise",
-                        type=float,
-                        default=1,
-                        help="Varience of noise")
-    parser.add_argument("-e", "--exp_lamb",
-                        type=float,
-                        default=0.2,
-                        help="Intensity of moments of trend change")
-    parser.add_argument("--seed",
-                        type=int,
-                        default=np.random.randint(low=0,
-                                                  high=100000,
-                                                  size=1)[0],
-                        help="Random seed")
-
-    return parser.parse_args()
-
-
-def dump_params(params):
-    import json
-    with open(os.path.join(params.outdir, "params.txt"), 'w') as f:
-        json.dump(vars(params), f)
-
-
-def plot(params, all_series):
-    import plotly.graph_objs as go
-    from plotly.offline import plot
-    traces = []
-    for series in all_series:
-        trace = go.Scatter(x=range(1, params.series_len + 1),
-                           y=series,
-                           mode='lines')
-        traces.append(trace)
-    layout = {"title": "Series"}
-    fig = {"data": traces, "layout": layout}
-    plot(fig)
-
-
-def get_seed(params):
-    if params.seed is not None:
-        np.random.seed(params.seed)
-    params.seed = np.random.randint(low=0,
-                                    high=100000,
-                                    size=params.number_series)
-    params.seed = list(params.seed)
-
-
-def prepare_output_dir(params):
-    smart_makedirs(params.outdir)
-
-
-def main(*args, **kwargs):
-    params = parse_cmd(*args, **kwargs)
-    prepare_output_dir(params)
-    get_seed(params)
-    dump_params(params)
-
-    all_series = []
-    for _ in xrange(params.number_series):
-        np.random.seed(params.seed[_])
-        wiener = get_wiener(N=params.series_len,
-                            sigma=params.var_noise)
-        trend = get_trend(N=params.series_len,
-                          lamb=params.exp_lamb,
-                          m_tan=params.mean_tang,
-                          v_tan=params.var_tang,
-                          sigma=params.var_noise,
-                          tr_start=params.trend_start)
-        series = get_series(trend, wiener)
-        all_series.append(series)
-        np.savetxt(os.path.join(params.outdir, str(_) + ".txt"), series)
-
-    if params.plot:
-        plot(params, all_series)
-
-
-if __name__ == "__main__":
-    main()
